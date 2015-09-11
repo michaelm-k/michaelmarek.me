587ad4693d3e1d7d32a9101151b4f254e2dfac4a
Michael Marek <atlikeross@gmail.com>
2015-09-10 19:03:35 -0500

    links.js swapped for just css

--- static/js/other.js
+++ static/js/other.js
@@ -1,24 +1,3 @@
 function popup(){
 	playerWindow = window.open("player", "player", "location=no,width=365,height=487");
-}		
-
-/* START: LEAVE WINDOW */
-function addEvent(obj, evt, fn) {
-    if (obj.addEventListener) {
-        obj.addEventListener(evt, fn, false);
-    }
-    else if (obj.attachEvent) {
-        obj.attachEvent("on" + evt, fn);
-    }
 }
-addEvent(window,"load",function(e) {
-    addEvent(document, "mouseout", function(e) {
-        e = e ? e : window.event;
-        var from = e.relatedTarget || e.toElement;
-        if (!from || from.nodeName == "HTML") {
-            $("span").stop(true).animate( {color: "#ffffff"}, "200" ); 
-			$("a").removeClass( "active" );
-        }
-    });
-});
-/* END: LEAVE WINDOW */
