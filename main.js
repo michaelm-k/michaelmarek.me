8020960002dde11fee1c02cf6db83921bc4cd0ed
Michael Marek <atlikeross@gmail.com>
2016-12-30 18:13:32 -0600

    This fucking link

--- main.js
+++ main.js
@@ -4,21 +4,22 @@ var Mincer = require('mincer');
 var csso = require('csso');
 var uglify = require('uglify-js');
 var http = require('http');
-
 var fs = require('fs');
 
 var app = express();
 var port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var mincerEnvironment = new Mincer.Environment();
 var environment = process.env.NODE_ENV || 'development';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
+var mincerEnvironment = new Mincer.Environment();
 mincerEnvironment.appendPath('assets');
 mincerEnvironment.appendPath('vendor');
+app.use('/assets', Mincer.createServer(mincerEnvironment));
+
 if (environment !== 'development') {
   mincerEnvironment.jsCompressor = function(context, data) {
     return uglify.minify(data, {fromString: true}).code;
@@ -27,7 +28,6 @@ if (environment !== 'development') {
     return csso.minify(data).css;
   };
 }
-app.use('/assets', Mincer.createServer(mincerEnvironment));
 
 if (environment === 'development') {
   require('./env.js');
@@ -67,68 +67,64 @@ var acceptedMimetypes = {'application/pdf': true,
                          'image/jpeg': true, 
                          'image/gif': true};
 
-app.get('/uploads', function(req, res) {
-  fs.readdir('assets/uploads', function(err, files) {
-    if (req.get('want') === process.env.UPLOAD_CODE) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(files));
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('What do you *want*?');
-    }  
-  }); 
-});
+app.route('/uploads')
+  .get(function(req, res) {
+    fs.readdir('assets/uploads', function(err, files) {
+      if (req.get('want') === process.env.UPLOAD_CODE) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(files));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('What do you *want*?');
+      }  
+    }); 
+  })
+  .post(function(req, res) {
+    var busboy = new Busboy({ headers: req.headers });
+    var files = [];
 
-app.post('/uploads', function(req, res) {
-  var busboy = new Busboy({ headers: req.headers });
-  var files = [];
-
-  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-    var currentChunks = [];
-    var validFile = fieldname === process.env.UPLOAD_CODE && acceptedMimetypes[mimetype];
-    file.on('data', function(data) {
-      if (validFile) {
-        currentChunks.push(data);
-      }      
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+      var currentChunks = [];
+      var validFile = fieldname === process.env.UPLOAD_CODE && acceptedMimetypes[mimetype];
+      file.on('data', function(data) {
+        if (validFile) {
+          currentChunks.push(data);
+        }      
+      });
+      file.on('end', function() {
+        if (validFile) {
+          files.push({ name: filename, data: Buffer.concat(currentChunks) });
+        }
+      });
     });
-    file.on('end', function() {
-      if (validFile) {
-        files.push({ name: filename, data: Buffer.concat(currentChunks) });
+    busboy.on('finish', function() {
+      res.writeHead(files.length === 0 ? 418 : 201, { 'Content-Type': 'text/plain', 'Connection': 'close' });
+      res.end(files.length === 0 ? "Who do you think you are?" : "Mk, I'll look the other way this time.");
+      for (var i=0; i<files.length; i++) {
+        fs.writeFile('assets/uploads/' + files[i].name, files[i].data, function(err) {});
+      }  
+    });
+    req.pipe(busboy);
+  })
+  .delete(function(req, res) {
+    var busboy = new Busboy({ headers: req.headers });
+    var filenames = [];
+
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      if (fieldname === process.env.UPLOAD_CODE) {
+        filenames.push(val);
       }
     });
+    busboy.on('finish', function() {
+      res.writeHead(filenames.length === 0 ? 418 : 200, { 'Content-Type': 'text/plain', 'Connection': 'close' });
+      res.end(filenames.length === 0 ? "Who do you think you are?" : "Mk, I'll look the other way this time.");
+      for (var i=0; i<filenames.length; i++) {
+        fs.unlink('assets/uploads/' + filenames[i], function(err) {});
+      }
+    });
+    req.pipe(busboy);
   });
-  busboy.on('finish', function() {
-    res.writeHead(files.length === 0 ? 418 : 201, { 'Content-Type': 'text/plain', 'Connection': 'close' });
-    res.end(files.length === 0 ? "Who do you think you are?" : "Mk, I'll look the other way this time.");
-    for (var i=0; i<files.length; i++) {
-      fs.writeFile('assets/uploads/' + files[i].name, files[i].data, function(err) {});
-    }  
-  });
-  req.pipe(busboy);
-});
 
-app.delete('/uploads', function(req, res) {
-  var busboy = new Busboy({ headers: req.headers });
-  var filenames = [];
-
-  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-    if (fieldname === process.env.UPLOAD_CODE) {
-      filenames.push(val);
-    }
-  });
-  busboy.on('finish', function() {
-    res.writeHead(filenames.length === 0 ? 418 : 200, { 'Content-Type': 'text/plain', 'Connection': 'close' });
-    res.end(filenames.length === 0 ? "Who do you think you are?" : "Mk, I'll look the other way this time.");
-    for (var i=0; i<filenames.length; i++) {
-      fs.unlink('assets/uploads/' + filenames[i], function(err) {});
-    }
-  });
-  req.pipe(busboy);
-});
-
-var run_server = server.listen(port, function () {
-  var host = run_server.address().address;
-  var port = run_server.address().port;
-
-  console.log('Listening at http://%s:%s', host, port);
+server.listen(port, function () {
+  console.log('Listening at http://%s:%s', this.address().address, this.address().port);
 });
